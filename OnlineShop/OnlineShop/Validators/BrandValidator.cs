7be12e45e90090065a5e4c1d0b33c08e033ecdf7
iamnovik <vlad09082003@mail.ru>
2023-11-19 21:05:13 +0300

    Set data types

--- OnlineShop/OnlineShop/Validators/BrandValidator.cs
+++ OnlineShop/OnlineShop/Validators/BrandValidator.cs
@@ -7,7 +7,7 @@ public class BrandValidator : AbstractValidator<Brand>
 {
     public BrandValidator()
     {
-        RuleFor(b => b.BrandName).NotEmpty().MaximumLength(50);
-        RuleFor(b => b.BrandId).NotEmpty();
+        RuleFor(b => b.BrandName).NotEmpty().MaximumLength(50).Matches("^[a-zA-Z0-9_-]+$");
+        RuleFor(b => b.BrandId).NotEmpty().InclusiveBetween(0,long.MaxValue);
     }
 }
